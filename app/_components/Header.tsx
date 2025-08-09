f9bcc0b3788b699f95c5ecae7a5d87a36b2cd235
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-08-09 11:38:24 +0530

    header prototype done

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -2,9 +2,23 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import React from 'react'
 
+const menuOptions = [
+  {
+    name : 'Home',
+    path : '/'
+  },
+  {
+    name : 'Pricing',
+    path : '/pricing'
+  },{
+    name : 'Contact Us',
+    path : '/contact-us'
+  }
+]
+
 function Header() {
   return (
-    <div>
+    <div className = 'flex justify-between px-3 items-center'>
       <div className = 'flex items-center gap-1'>
         <Image
           src = {'/logo.png'}
@@ -13,13 +27,27 @@ function Header() {
           height = {50}
           className = 'p-2 h-[45px] w-[45px]'
         />
-        <h2 className = 'font-bold text-xl tracking-wide'>
+        <h2 className = 'font-bold text-2xl tracking-wide'>
           Nomigo
         </h2>
-        <Button>
-          Get Started
-        </Button>
       </div>
+      <div className = 'flex justify-center items-center gap-4'>
+        {
+          menuOptions.map((item , idx) => (
+            <div 
+              key = {idx}
+              className = 'font-semibold text-md hover:scale-105 transition-al cursor-pointer hover:border-b hover:border-[#687864]'
+            >
+              {item.name}
+            </div>
+          ))
+        }
+      </div>
+      <Button
+       className = 'cursor-pointer'
+      >
+        Get Started
+      </Button>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2 className = 'font-bold text-4xl'>Hey, welcome to the nomigo</h2>
+    
     </div>
   );
 }
