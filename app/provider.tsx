b271a142fd41a7edd30c4567a744ed23439ecfb3
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-08-10 10:36:19 +0530

    added convex api to create new user

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Header from './_components/Header';
-import Hero from './_components/Hero';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useUser } from '@clerk/nextjs';
 
 function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const createUser  = useMutation(api.user.CreateNewUser);
+
+  const {user} = useUser();
+
+  const createNewUser = async () => {
+    if(user){
+      const res = await createUser({
+        email : user?.primaryEmailAddress?.emailAddress + "",
+        imageUrl : user?.imageUrl,
+        name : user?.fullName + "",
+      });
+      // console.log(res);
+    }
+  }
+
+  useEffect(() => {
+    user && createNewUser()
+  } , [user]);
+
   return (
     <div>
       <Header/>
---
+++ convex/user.ts
@@ -0,0 +1,29 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { q } from "motion/react-client";
+
+export const CreateNewUser = mutation({
+  args : {
+    name : v.string(),
+    email : v.string(),
+    imageUrl : v.string(),
+  },
+  handler : async (ctx , args) => {
+    // user already exists 
+    const user = await ctx.db.query('users').
+    filter((q) => q.eq(q.field('email') , args.email))
+    .collect();
+
+    if(!(user?.length)){
+     const userData = {
+      name : args.name,
+      email : args.email,
+      imageUrl : args.imageUrl,
+     }
+
+     const res = await ctx.db.insert('users' , userData);
+     return userData;
+    }
+    return user[0];
+  }
+})
