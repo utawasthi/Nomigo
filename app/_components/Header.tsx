3ed33299c1f19368301fc42003ed4327f0154948
Utkarsh Awasthi <utkarshawasthi100@gmail.com>
2025-08-13 19:21:27 +0530

    made the header sticky

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -24,58 +24,60 @@ function Header() {
   console.log(path);
 
   return (
-    <div className = 'flex justify-between px-3 items-center border-b-1'>
-      <div className = 'flex items-center gap-1'>
-        <Image
-          src = {'/logo.png'}
-          alt = {'logo'}
-          width = {70}
-          height = {70}
-          className = 'object-contain'
-        />
-        <h2 className = 'font-extrabold text-3xl text-[#687864]'>
-          Nomigo
-        </h2>
-      </div>
-      <div className = 'flex justify-center items-center gap-4'>
+    <div className="sticky top-0 z-50 bg-white/30 backdrop-blur-md border-b border-white/20">
+      <div className = 'flex justify-between px-3 items-center border-b-1'>
+        <div className = 'flex items-center gap-1'>
+          <Image
+            src = {'/logo.png'}
+            alt = {'logo'}
+            width = {70}
+            height = {70}
+            className = 'object-contain'
+          />
+          <h2 className = 'font-extrabold text-3xl text-[#687864]'>
+            Nomigo
+          </h2>
+        </div>
+        <div className = 'flex justify-center items-center gap-4'>
+          {
+            menuOptions.map((item , idx) => (
+              <Link
+                href = {item.path} 
+                key = {idx}
+                className = 'font-semibold text-md hover:scale-105 transition-al cursor-pointer hover:border-b hover:border-[#687864]'
+              >
+                {item.name}
+              </Link>
+            ))
+          }
+        </div>
         {
-          menuOptions.map((item , idx) => (
-            <Link
-              href = {item.path} 
-              key = {idx}
-              className = 'font-semibold text-md hover:scale-105 transition-al cursor-pointer hover:border-b hover:border-[#687864]'
+          !user ? <SignInButton mode = 'modal'>
+            <Button
+              className = 'cursor-pointer'
             >
-              {item.name}
-            </Link>
-          ))
+              Get Started
+            </Button>
+          </SignInButton>
+          : 
+          path === '/create-new-trip' ?  
+          <Link href = {'/my-trips'}>
+            <Button
+              className = 'cursor-pointer'
+            >
+              My Trips
+            </Button>
+          </Link>
+          :
+          <Link href = {'/create-new-trip'}>
+            <Button
+              className = 'cursor-pointer'
+            >
+              Create New Trip
+            </Button>
+          </Link>
         }
       </div>
-      {
-        !user ? <SignInButton mode = 'modal'>
-          <Button
-            className = 'cursor-pointer'
-          >
-            Get Started
-          </Button>
-        </SignInButton>
-        : 
-        path === '/create-new-trip' ?  
-         <Link href = {'/my-trips'}>
-          <Button
-            className = 'cursor-pointer'
-          >
-            My Trips
-          </Button>
-        </Link>
-        :
-        <Link href = {'/create-new-trip'}>
-          <Button
-            className = 'cursor-pointer'
-          >
-            Create New Trip
-          </Button>
-        </Link>
-      }
     </div>
   )
 }
